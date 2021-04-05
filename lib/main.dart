d8839eb9d0f281c8ef603fc110e683f9d33070eb
Suraj <surajjadhav525k@gmail.com>
2021-04-05 15:02:32 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,28 @@ import 'package:image_collage_widget/image_collage_widget.dart';
 import 'package:image_collage_widget/utils/CollageType.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("Collage"),
+  runApp(Mapp());
+}
+
+class Mapp extends StatefulWidget {
+  @override
+  _MappState createState() => _MappState();
+}
+
+class _MappState extends State<Mapp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Collage"),
+        ),
+        body: ImageCollageWidget(
+          collageType: CollageType.CenterBig,
+          withImage: true,
+          //   filePath: _directory?.path?.toString(),
+        ),
       ),
-      body: ImageCollageWidget(
-        collageType: CollageType.CenterBig,
-        withImage: true,
-        //   filePath: _directory?.path?.toString(),
-      ),
-    ),
-  ));
+    );
+  }
 }
